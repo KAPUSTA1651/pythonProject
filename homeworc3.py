b2399bf0bf72d51fb0366325bed2af2aba5d6c8c
KAPUSTA1651 <romario19112008@yandex.ru>
2023-10-31 18:06:44 +0300

    Это дз, третий комит

--- homeworc3.py
+++ homeworc3.py
@@ -31,12 +31,12 @@
 # else:
 #     print(Fore.BLACK+Back.RED+"ошибка вы ввели неверный знак"+Style.RESET_ALL)
 # #
-# a=input('введите пороль: ')
-# if len(a) >= 8 and not a.isupper() and not a.islower() and not a.isalpha() and not a.isdigit():
-#     print('сложный')
-# elif len(a) >= 8 and a.islower() and not a.isalpha():
-#     print('средний')
-# elif len(a) >= 8 and (a.isalpha() or a.isdigit()):
-#     print('слабый')
-# else:
-#     print('очень слабый')
+a=input('введите пороль: ')
+if len(a) >= 8 and not a.isupper() and not a.islower() and not a.isalpha() and not a.isdigit():
+    print('сложный')
+elif len(a) >= 8 and a.islower() and not a.isalpha():
+    print('средний')
+elif len(a) >= 8 and (a.isalpha() or a.isdigit()):
+    print('слабый')
+else:
+    print('очень слабый')
