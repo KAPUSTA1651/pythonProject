09d6a5051d0b7cb37e88507e59b1617f9f9f9e9c
KAPUSTA1651 <romario19112008@yandex.ru>
2023-11-02 17:10:57 +0300

    дз

--- homeworc3.py
+++ homeworc3.py
@@ -31,12 +31,22 @@
 # else:
 #     print(Fore.BLACK+Back.RED+"ошибка вы ввели неверный знак"+Style.RESET_ALL)
 # #
-a=input('введите пороль: ')
-if len(a) >= 8 and not a.isupper() and not a.islower() and not a.isalpha() and not a.isdigit():
-    print('сложный')
-elif len(a) >= 8 and a.islower() and not a.isalpha():
-    print('средний')
-elif len(a) >= 8 and (a.isalpha() or a.isdigit()):
-    print('слабый')
-else:
-    print('очень слабый')
+# a=input('введите пороль: ')
+# if len(a) >= 8 and not a.isupper() and not a.islower() and not a.isalpha() and not a.isdigit():
+#     print('сложный')
+# elif len(a) >= 8 and a.islower() and not a.isalpha():
+#     print('средний')
+# elif len(a) >= 8 and (a.isalpha() or a.isdigit()):
+#     print('слабый')
+# else:
+#     print('очень слабый')
+
+
+def x():
+    a=int(input())
+    if a>25 and a%2==0:
+        print(1)
+    else:
+        print(0)
+        return
+x()
